513b1c475d868aed9bf9de6f6ec362885754227f
Manoj <manojkurana@outlook.com>
2023-10-09 03:35:50 +0530

    Package Name Renamed

--- src/test/java/com/softwaretestingo/launch/LaunchChromeTest.java
+++ src/test/java/com/softwaretestingo/launch/LaunchChromeTest.java
@@ -1,4 +1,4 @@
-package com.softwaretestingo;
+package com.softwaretestingo.launch;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
--- src/test/java/com/softwaretestingo/launch/LaunchFreFoxTest.java
+++ src/test/java/com/softwaretestingo/launch/LaunchFreFoxTest.java
@@ -1,4 +1,4 @@
-package com.softwaretestingo;
+package com.softwaretestingo.launch;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
